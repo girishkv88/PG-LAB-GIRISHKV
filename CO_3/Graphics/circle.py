42e0f10af446a81abe38f49f245c975d496e5434
Girish k v <73742657+girishkv88@users.noreply.github.com>
2022-01-30 21:31:51 +0530

    Delete CO_3/Graphics directory

--- CO_3/Graphics/circle.py
+++
@@ -1,8 +0,0 @@
-pi =3.14
-def c_area(r):
-    area=pi*r*r
-    print("AREA OF CIRCLE =",area)
-def c_perimeter(r):
-    perimeter=2*pi*r
-    print("PERIMETER OF CIRCLE =",perimeter)
-
--- CO_3/Graphics/rectangle.py
+++
@@ -1,6 +0,0 @@
-def r_area(l,b):
-    area=l*b
-    print("AREA OF RECTANGLE =",area)
-def r_perimeter(l,b):
-    perimeter=2*(l+b)
-    print("PEREMETER OF RECTANGLE =",perimeter)
